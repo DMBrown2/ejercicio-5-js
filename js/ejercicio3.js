37801fd7fabb20df6df444858417070ceca378f4
Daniela Brown <daniimbrown@gmail.com>
2024-11-25 16:53:51 -0300

    finalizado ejercicio 3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,6 +1,22 @@
 //3.Solicita al usuario dos números y una operación (+, -, *, /) usando prompt. Muestra el resultado de la operación en un alert.
+
 let numero1 = parseFloat(prompt("Ingrese un número"))
 let operacion = prompt("Ingrese una operación (+, -, *, /)")
 let numero2 = parseFloat(prompt("Ingrese otro número"))
 
-alert(numero1 + operacion + numero2)
+let resultado
+
+if(operacion === "+") {
+    resultado = numero1 + numero2
+} else if (operacion === "-") {
+    resultado = numero1 - numero2
+} else if(operacion === "*") {
+    resultado = numero1 * numero2
+} else if(operacion === "/") {
+    numero1 / numero2
+} else {
+    resultado = ("Error de operación")
+}
+
+alert(`El resultado es: ${resultado}`)
+
